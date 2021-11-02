6bba75a81b87c7658d8869c21fcc25f8a9fbe95f
Leonardo Sousa <38815972+LeoRSousa@users.noreply.github.com>
2021-11-02 01:03:26 -0300

    Note_021121
    Create client inicial

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,14 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 import Login from "../pages/Login";
 // import Home from "./Home";
+import CreateClient from "../pages/client/create";
 
 const Routes = () => {
     return (
         <BrowserRouter >
             < Route component={Login} path="/" exact />
             {/* < Route component={Home} path="/home" /> */}
+            < Route component={CreateClient} path="/client/create" exact />
         </BrowserRouter>
     )
 }
